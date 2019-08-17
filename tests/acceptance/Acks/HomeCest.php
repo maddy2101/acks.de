a767e168956c6fc5b812d3afe740db36062349fc
Anja Leichsenring <anja.leichsenring@typo3.com>
2019-08-17 14:20:41 +0200

    [FEATURE] Set up frontend testing

---
+++ tests/acceptance/Acks/HomeCest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AbSoftlab\Tests\Acks;
+
+
+use AcceptanceTester;
+
+class HomeCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('index.php?id=1');
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->canSee('[AcKs] - Ancient Knights - Wir brauchen keine Logik!');
+    }
+}
---
+++ tests/acceptance/Since1988/HomeCest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AbSoftlab\Tests\Since1988;
+use AcceptanceTester;
+
+class HomeCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('index.php?id=28');
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->canSee('WM 2018 - Endstand');
+    }
+}
